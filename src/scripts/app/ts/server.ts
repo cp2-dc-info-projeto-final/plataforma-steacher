1f94c18a82d150422809532d688a12c19d8d28a4
Leonardo Henrique <leonardolopesinf@gmail.com>
2019-05-29 00:46:25 -0300

    adicionando servidor

---
+++ src/scripts/app/ts/server.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000.'));
+
+app.get('/', (req, resp) =>{
+    resp.send(
+        `
+        <!DOCTYPE html>
+        <html lang="pr-br">
+        <head>
+            <meta charset="UTF-8">
+            <title>Testando server...</title>
+        </head>
+        <body>
+            <h1>Testando server...</h1>
+        </body>
+        </html>
+        `
+    )
+})
