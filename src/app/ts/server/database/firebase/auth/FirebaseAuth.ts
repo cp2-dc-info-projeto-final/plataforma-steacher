4a8708ba6cfd65b88eaf1bc2cee54338674ea41d
Leonardo Henrique <leonardolopesinf@gmail.com>
2019-07-24 01:43:33 -0300

    adicinando rascunho FirebaseAuth

---
+++ src/app/ts/server/database/firebase/auth/FirebaseAuth.ts
@@ -0,0 +1,19 @@
+import firebase from '../firebase';
+
+export default class FirebaseAuth{
+
+    private _auth = firebase.auth()
+
+    public signUp(){
+
+    }
+    
+    public signIn(){
+
+    }
+
+    public signOut(){
+
+    }
+
+}
