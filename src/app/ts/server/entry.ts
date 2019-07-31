50140c96ae99f65a8008f1ac3b1b9e72d28e906b
Leonardo Henrique <leonardolopesinf@gmail.com>
2019-07-30 21:53:07 -0300

    Mudando cor mensagem

--- src/app/ts/server/entry.ts
+++ src/app/ts/server/entry.ts
@@ -1,8 +1,8 @@
 /*---------------Importações---------------*/
 
 import app from './config/express';
-import { message, error } from './config/colors';
+import { init } from './config/colors';
 
 /*----------------Aplicação----------------*/
 
-app.listen(3000, () => console.log(error("Servidor rodando na porta 3000.")));
+app.listen(3000, () => console.log(init("Servidor rodando na porta 3000.")));
