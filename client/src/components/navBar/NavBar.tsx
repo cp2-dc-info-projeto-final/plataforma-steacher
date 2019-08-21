bce700918fd092ab933de242511fb6e3155454e2
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-08-20 23:06:14 -0300

    Navbar editado

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -4,18 +4,12 @@ import 'bulma/css/bulma.min.css';
 
 //REMOVER TOOS OS HREFS
 
-
 export default function NavBar() {
     return (
         <nav className="navbar is-black level" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand level-item has-text-centered" style={{maxHeight: '4.2rem'}}>
-                <a className="navbar-item" href="https://bulma.io">
-                    <img src={process.env.PUBLIC_URL + 'sistch2.png'} alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="85" style={{maxHeight: '3.8rem'}}/>
-                </a>
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
+            <div className="navbar-brand level-item has-text-centered" style={{maxHeight: '4rem'}}>
+                <a href="/">
+                    <img src={process.env.PUBLIC_URL + 'assets/logo.png'} alt="Steacher logo" width="85" style={{maxHeight: '3.5rem', }}/>
                 </a>
             </div>
         </nav>
