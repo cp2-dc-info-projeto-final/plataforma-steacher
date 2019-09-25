161040a4c158596c44cca8dab4ca467cf824e979
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-09-25 05:08:01 -0300

    Nova interface grafica da pagina de login

--- client/gulpfile.ts
+++
@@ -1,9 +0,0 @@
-import * as gulp from 'gulp';
-import * as imagemin from 'gulp-imagemin';
-
-gulp.task('build-assets', function() {
-
-  gulp.src('public/assets/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('public/assets'));
-});
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import 'materialize-css/dist/css/materialize.min.css';
+
 import { Background } from '../App.style';
+import NavBar from '../components/navBar/NavBar';
+import Card from '../components/auth/card/Card';
+import LinkButton from '../components/auth/inputs/LinkButton';
 
 export default function Login() {
 
   return (
     <Background>
-
+      <div className="row">
+        <NavBar titulo="" />
+      </div>
+      <div className="row">
+        <div className="col offset-m2 offset-l2 s12 m8 l8" style={{marginTop:"-3%"}}>
+          <Card form="login" />
+        </div>
+        <div className="col offset-m4 offset-l4 s4 m4 l4 center">
+          <LinkButton link="/cadastro" label="Cadastre-se" width="80%"></LinkButton>
+        </div>
+      </div>
     </Background>
   );
 }
