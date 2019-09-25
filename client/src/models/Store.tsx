fa665b31e01c317a0b32a1566e9d0ec33550da64
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-09-25 05:06:49 -0300

    Adicionando novas interfaces do storage

--- client/src/models/Store.tsx
+++ client/src/models/Store.tsx
@@ -3,13 +3,25 @@ interface LoginState{
     password: string,
 }
 
-interface PasswordState{
-    visibility: boolean
+interface CadastroState{
+    name: string,
+    subname: string,
+    registration: string,
+    email: string,
+    emailConfirm: string,
+    password: string,
+    passwordConfirm: string,
+    accountType: string
+}
+
+interface AuthState{
+    redirect: boolean
 }
 
 export interface State{
     login: LoginState,
-    password: PasswordState
+    cadastro: CadastroState
+    auth: AuthState
 }
 
 export interface Action{
