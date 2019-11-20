6e6a569700aa63fb19ec39912b9668c523b5ac2f
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-11-20 01:07:08 -0300

    [REACT] Adicionando area de mensagens de grupo

---
+++ client/src/pages/home/messageArea/TurmaArea.tsx
@@ -0,0 +1,107 @@
+//#region Npm
+
+import React from 'react';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+// import ScrollArea from 'react-scrollbar';
+// import 'react-scrollbar/dist/no-css'
+// import PerfectScrollbar from 'react-perfect-scrollbar'
+// import 'react-perfect-scrollbar/dist/css/styles.css';
+// import { Scrollbar } from 'react-scrollbars-custom';
+
+
+//#endregion
+
+//#region Actions
+
+import { useSelector, useDispatch } from 'react-redux';
+import ChatBox from './chatBox/ChatBox';
+
+//#endregion
+
+//#region Functions
+
+
+
+//#endregion
+
+//#region Interfaces
+
+
+
+//#endregion
+
+//#region Components
+
+
+
+//#endregion
+
+//#region Styles
+
+import './TurmaArea.css';
+
+//#endregion
+
+//#region Propriedades
+
+type Props = {
+
+}
+
+//#endregion
+
+export default function TurmaArea(props: Props) {
+    const dispatch = useDispatch();
+
+    //#region States
+
+
+
+    //#endregion
+
+    //#region OnChanges
+
+
+
+    //#endregion
+
+    //#region OnClicks
+
+
+
+    //#endregion
+
+    //#region XML
+
+    return (
+        <div className="" style={{marginTop: "-2%"}}>
+            <ul className="collection" style={{ marginBottom: 0, marginTop: 0 }}>
+                <ul className="collapsible">
+                    <li>
+                        <div className="collapsible-header"><span className="flow-text">IN303</span></div>
+                        <div className="collapsible-body" style={{ padding: "0px" }}>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="collapsible-header"><span className="flow-text">IN303</span></div>
+                        <div className="collapsible-body" style={{ padding: "0px" }}>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                            <ChatBox></ChatBox>
+                        </div>
+                    </li>
+                </ul>
+            </ul>
+        </div>
+    );
+
+    //#endregion
+}
