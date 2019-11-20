4793b26f29b0d5b2b5f914dd062cbc675d0f5d9c
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-11-20 00:54:38 -0300

    [REACT-ROUTER-DOM] Adicionando rota privada

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import PasswordRecover from './pages/PasswordRecover';
+import { useSelector } from 'react-redux';
+import { State } from './models/Store';
 
-const Routes = () =>(
+const PrivateRoute = ({ component: Component, ...rest }: any): any => {
+    const isLogged = useSelector((state: State) => state.auth.auth);
+    
+    return <Route
+        {...rest}
+        render={props => isLogged ? (<Component {...props} />) : <Redirect to={{ pathname: '/', state: { from: props.location } }} />}
+    />
+}
+
+const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route exact path="/cadastro" component={Cadastro}/>
-            <Route exact path="/home" component={Home}/>
-            <Route exact path="/passwordRecover" component={PasswordRecover}/>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/cadastro" component={Cadastro} />
+            <Route exact path="/passwordRecover" component={PasswordRecover} />
+            {/* <PrivateRoute path="/home" component={Home} /> */}
+            <Route exact path="/home" component={Home} />
         </Switch>
     </BrowserRouter>
 )
